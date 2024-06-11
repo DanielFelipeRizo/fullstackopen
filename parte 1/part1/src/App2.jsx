486d1db4214c21a33d89c0457004f5e84c9a4d6f
DanielRizo <danielrizo@wposs.com>
2024-06-11 14:27:03 -0500

    parte 1

--- parte 1/part1/src/App2.jsx
+++ parte 1/part1/src/App2.jsx
@@ -12,19 +12,49 @@ const App2 = () => {
         'The only way to go fast, is to go well.'
     ]
 
-    const [selected, setSelected] = useState(0)
+    const [selected, setSelected] = useState(0);
+    const points = { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0 };
+    const [selectedPoints, setSelectedPoints] = useState(points);
+    const copyPoints = { ...selectedPoints };
+    let valVotes = false;
 
     const handleClickRandom = () => {
         const RandomNumber = Math.floor(Math.random() * 7) + 1;
-        console.log(RandomNumber);
+        //console.log(RandomNumber);
         setSelected(RandomNumber);
+    }
+
+    const handleClickVote = () => {
+        copyPoints[selected] += 1;
+        setSelectedPoints(copyPoints)
+        valVotes = true;
+        console.log('copyPoints', copyPoints);
+    }
+
+    function mostVotes() {
+        let maxProp = null;
+        let maxVal = -Infinity;
+
+        for (const prop in copyPoints) {
+
+            if (copyPoints[prop] > maxVal) {
+                maxVal = copyPoints[prop];
+                maxProp = prop;
+            }
+        }
+        return maxProp;
     }
 
     return (
         <div>
             {anecdotes[selected]}
             <h1>valor: {selected}</h1>
+            <h3>has {copyPoints[selected]} votes</h3>
+            <button onClick={handleClickVote}>Vote</button>
             <button onClick={handleClickRandom}>Next anecdote</button>
+            <h3>Anecdote With Most Votes:</h3>
+            <p>{anecdotes[mostVotes()]}</p>
+            <p>has {copyPoints[mostVotes()]} votes</p>
         </div>
     )
 }
