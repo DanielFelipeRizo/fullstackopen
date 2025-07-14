b5388d1783b592cc10f9841063e63d97446491bd
DanielFelipeRizo <dfrizop@ufpso.edu.co>
2025-07-13 21:06:49 -0500

    avances

---
+++ parte8/8.13 - 8.16/src/App.jsx
@@ -0,0 +1,50 @@
+import { useQuery } from '@apollo/client';
+import { useState } from "react";
+import Authors from "./components/Authors";
+import Books from "./components/Books";
+import NewBook from "./components/NewBook";
+import Notify from "./components/Notify";
+import { All_AUTHORS, ALL_BOOKS } from "./queries";
+
+const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [page, setPage] = useState("authors");
+  const result_all_authors = useQuery(All_AUTHORS);
+  const result_all_books = useQuery(ALL_BOOKS);
+
+
+  if (result_all_authors.loading || result_all_books.loading) {
+    return <div>loading...</div>;
+  }
+
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
+  // console.log('authors', result_all_authors.data.allAuthors);
+  // console.log('books', result_all_books.data.allBooks);
+
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setPage("authors")}>authors</button>
+        <button onClick={() => setPage("books")}>books</button>
+        <button onClick={() => setPage("add")}>add book</button>
+      </div>
+
+      <Notify errorMessage={errorMessage} />
+
+      <Authors show={page === "authors"} authors={result_all_authors.data.allAuthors} setError={notify} />
+
+      <Books show={page === "books"} books={result_all_books.data.allBooks} />
+
+      <NewBook show={page === "add"} setError={notify} />
+    </div>
+  );
+};
+
+export default App;
---
+++ parte8/8.13 - 8.16/src/components/Authors.jsx
@@ -0,0 +1,110 @@
+import { useState, useEffect } from 'react'
+import { useMutation } from '@apollo/client'
+import { EDIT_BORN_AUTHOR } from '../queries'
+import Select from 'react-select'
+import customStyles from '../styles/selectStyles'
+
+const Authors = ({ authors, show, setError }) => {
+
+  const [name, setName] = useState('')
+  const [setBornTo, setBorn] = useState('')
+
+  const [changeBorn, result] = useMutation(EDIT_BORN_AUTHOR)
+
+  useEffect(() => {
+    if (result.data && result.data.editAuthor === null) {
+      setError('Author not found')
+    }
+  }, [result.data])
+
+  if (!show) {
+    return null
+  }
+
+  const submit = async (event) => {
+    event.preventDefault()
+
+    changeBorn({
+      variables: { name, setBornTo: parseInt(setBornTo) }
+    })
+
+    // console.log('result', result);
+
+    setName('')
+    setBorn('')
+  }
+
+  // convertir los autores a formato compatible con react-select
+  const authorOptions = authors
+    .slice(0, 10) // limitar a 10
+    .map((author) => ({
+      value: author.name,
+      label: author.name,
+    }))
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>born</th>
+            <th>books</th>
+          </tr>
+          {authors.map((a) => (
+            <tr key={a.id}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <h3>Set birthyear</h3>
+
+      <form onSubmit={submit}>
+
+
+        <div>
+          <label>Author</label>
+          <Select
+            options={authorOptions}
+            onChange={(selectedOption) => setName(selectedOption.value)}
+            placeholder="Selecciona un autor..."
+            isSearchable
+            styles={customStyles}
+          />
+        </div>
+
+        <div>
+          born
+          <input
+            type='number'
+            value={setBornTo}
+            onChange={({ target }) => setBorn(target.value)}
+          />
+        </div>
+        <button type="submit">update author</button>
+      </form>
+
+    </div>
+  )
+}
+
+import PropTypes from 'prop-types'
+
+Authors.propTypes = {
+  show: PropTypes.bool,
+  setError: PropTypes.func,
+  authors: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      born: PropTypes.number,
+      bookCount: PropTypes.number
+    })
+  ).isRequired
+}
+
+export default Authors
