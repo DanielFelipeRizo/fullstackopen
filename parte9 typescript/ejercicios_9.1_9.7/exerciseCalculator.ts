25993369d62ef6ddccdddfb18629a0a3db024cf0
DanielFelipeRizo <dfrizop@ufpso.edu.co>
2025-10-12 17:08:36 -0500

    ejercicios hasta el 9.7 completados

--- parte9 typescript/ejercicios_9.1_9.7/exerciseCalculator.ts
+++ parte9 typescript/ejercicios_9.1_9.7/exerciseCalculator.ts
@@ -9,7 +9,6 @@ interface exerciseValues {
 }
 
 const parseArrArguments = (args: string[]): number[] => {
-
   if (args.length < 3) throw new Error("Not enough arguments");
 
   // Tomamos todos los argumentos desde la posición 2 en adelante
@@ -17,15 +16,21 @@ const parseArrArguments = (args: string[]): number[] => {
     const value = Number(arg);
 
     if (isNaN(value)) {
-      throw new Error(`Argument at position ${index + 2} ('${arg}') is not a number`);
+      throw new Error(
+        `Argument at position ${index + 2} ('${arg}') is not a number`
+      );
     }
 
     if (value > 24) {
-      throw new Error(`Argument at position ${index + 2} ('${arg}') exceeds 24`);
+      throw new Error(
+        `Argument at position ${index + 2} ('${arg}') exceeds 24`
+      );
     }
 
     if (value < 0) {
-      throw new Error(`Argument at position ${index + 2} ('${arg}') is negative`);
+      throw new Error(
+        `Argument at position ${index + 2} ('${arg}') is negative`
+      );
     }
 
     return value;
@@ -34,36 +39,32 @@ const parseArrArguments = (args: string[]): number[] => {
   return arrDays;
 };
 
-
-
-const calculator = (arrDays: number[]): exerciseValues => {
-
+export const calculator = (arrDays: number[]): exerciseValues => {
   // const arrDays: number[] = arrValues;
   const target: number = arrDays.pop() || 0;
 
   const periodLength: number = arrDays.length;
 
-  const trainingDays: number = (arrDays.filter(d => d > 0).length);
+  const trainingDays: number = arrDays.filter((d) => d > 0).length;
 
   const average: number = arrDays.reduce((a, b) => a + b, 0) / periodLength;
 
   type Rating = 1 | 2 | 3;
 
   let rating: Rating = 1;
-  let ratingDescription: string = '';
+  let ratingDescription: string = "";
   let success: boolean = false;
 
   if (average >= target) {
     rating = 3;
-    ratingDescription = 'Muy bien, objetivo cumplido';
+    ratingDescription = "Muy bien, objetivo cumplido";
     success = true;
   } else if (average >= target / 2) {
     rating = 2;
-    ratingDescription = 'No está mal, pero podrías hacerlo mejor';
-  }
-  else if (average < target / 2) {
+    ratingDescription = "No está mal, pero podrías hacerlo mejor";
+  } else if (average < target / 2) {
     rating = 1;
-    ratingDescription = 'Vamos, que no has hecho nada';
+    ratingDescription = "Vamos, que no has hecho nada";
   }
 
   return {
@@ -75,7 +76,6 @@ const calculator = (arrDays: number[]): exerciseValues => {
     target: target,
     average: average,
   };
-
 };
 
 try {
--- parte9 typescript/ejercicios_9.1_9.7/index.ts
+++ parte9 typescript/ejercicios_9.1_9.7/index.ts
@@ -1,32 +1,31 @@
-import express from 'express';
-import { bmiCalculator, parseArgumentsUrl } from '../ejercicios_9.1_9.7/bmiCalculator';
+import express from "express";
+import {
+  bmiCalculator,
+  parseArgumentsUrl,
+} from "../ejercicios_9.1_9.7/bmiCalculator";
+import { calculator } from "./exerciseCalculator";
+
 const app = express();
 
-app.get('/ping', (_req, res) => {
-  res.send('pong');
+app.get("/ping", (_req, res) => {
+  res.send("pong");
 });
-``
-app.get('/hello', (_req, res) => {
-  res.send('Hello Full Stack!');
+``;
+app.get("/hello", (_req, res) => {
+  res.send("Hello Full Stack!");
 });
 
-app.get('/bmi/', (req, res) => {
-
+app.get("/bmi/", (req, res) => {
   if (!req.query.weight || !req.query.height) {
-    res.status(400).send({ error: 'malformatted parameters' });
+    res.status(400).send({ error: "malformatted parameters" });
     return;
   }
 
   try {
     const { weight, height } = req.query;
     const { value1, value2 } = parseArgumentsUrl([weight, height] as string[]);
-    const resultBmi = bmiCalculator(
-      Number(value1),
-      Number(value2)
-    );
-    console.log('---------->', resultBmi.category);
+    const resultBmi = bmiCalculator(Number(value1), Number(value2));
     res.send({ weight, height, bmi: resultBmi.bmi });
-
   } catch (error: unknown) {
     let errorMessage = "Something bad happened.";
     if (error instanceof Error) {
@@ -37,7 +36,39 @@ app.get('/bmi/', (req, res) => {
   }
 });
 
+app.post("/exercises", express.json(), (req, res) => {
+  const { daily_exercises, target } = req.body;
+  if (!daily_exercises || !target) {
+    res.status(400).send({ error: "parameters missing" });
+    return;
+  }
 
+  if (!Array.isArray(daily_exercises) || isNaN(Number(target))) {
+    res.status(400).send({ error: "malformatted parameters" });
+    return;
+  }
+
+  try {
+    const arrDailyExercisesAndTarget = [...daily_exercises, target].map((d) =>
+      Number(d)
+    );
+
+    if (arrDailyExercisesAndTarget.some(isNaN)) {
+      throw new Error("malformatted parameters");
+    }
+
+    const result = calculator(arrDailyExercisesAndTarget);
+    res.send(result);
+    
+  } catch (error: unknown) {
+    let errorMessage = "Something bad happened.";
+    if (error instanceof Error) {
+      errorMessage += " Error: " + error.message;
+      res.status(400).send({ error: errorMessage });
+      console.log(errorMessage);
+    }
+  }
+});
 
 const PORT = 3003;
 
