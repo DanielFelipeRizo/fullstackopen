eb9fb609948db963caa5ad718f443af30540ca6f
DanielRizo <danielrizo@wposs.com>
2025-10-17 16:51:44 -0500

    avances

--- parte9 typescript/diarios de vuelo/src/services/diaryService.ts
+++ parte9 typescript/diarios de vuelo/src/services/diaryService.ts
@@ -1,5 +1,5 @@
 import diaries from '../../data/diaries.js';
-import type { NonSensitiveDiaryEntry, DiaryEntry } from '../types.js';
+import type { NonSensitiveDiaryEntry, DiaryEntry, NewDiaryEntry } from '../types.js';
 
 
 const getEntries = (): DiaryEntry[] => {
@@ -15,12 +15,25 @@ const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
   }));
 };
 
-const addEntry = () => {
-  return null;
+const findById = (id: number): DiaryEntry | undefined => {
+  const entry = diaries.find(d => d.id === id);
+  return entry;
+};
+
+const addEntry = ( entry: NewDiaryEntry ): DiaryEntry => {
+
+  const newDiaryEntry = {
+    id: Math.max(...diaries.map(d => d.id)) + 1,
+    ...entry
+  }
+
+  diaries.push(newDiaryEntry);
+  return newDiaryEntry;
 };
 
 export default {
   getEntries,
   addEntry,
-  getNonSensitiveEntries
+  getNonSensitiveEntries,
+  findById
 };
---
+++ parte9 typescript/diarios de vuelo/src/utils.ts
@@ -0,0 +1,27 @@
+import type { NewDiaryEntry } from './types.js';
+
+const toNewDiaryEntry = (object: unknown): NewDiaryEntry => {
+
+    console.log(object); // now object is no longer unused
+    const newEntry: NewDiaryEntry = {
+        weather: 'cloudy', // fake the return value
+        visibility: 'great',
+        date: '2022-1-1',
+        comment: 'fake news'
+    };
+    return newEntry;
+}
+
+const parseComment = (comment: unknown): string => {
+  if (!comment || !isString(comment)) {
+    throw new Error('Incorrect or missing comment');
+  }
+
+  return comment;
+};
+
+const isString = (text: unknown): text is string => {
+  return typeof text === 'string' || text instanceof String;
+};
+
+export default toNewDiaryEntry;
